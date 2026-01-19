96dc272e1ef0932104982f5a9d7423d9b1cf16ab
Williamsanchez056 <williams_sanchez@ucne.edu.do>
2026-01-19 18:32:26 -0400

    Este es mi primer cambio

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora{
+    public static void main(String [] args){
+        int a = 2;
+        int b = 3;
+        int c = 15;
+        int d = 70;
+        int e = 80;
+
+        int resultado = a + b + c + d + e;
+
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+    
+}
